aeef036723e1be1298d70504377bce50a16f318d
Eoinknd16 <42419154+Eoinknd16@users.noreply.github.com>
2018-08-15 23:03:07 +0100

    Update ScientificCalculator.py

--- ScientificCalculator.py
+++ ScientificCalculator.py
@@ -1,33 +1,57 @@
+#### CODE IS GLITCH FIX IF YOU CAN ####
+
+
 def main():
 
+#### defining all the functions.
 
     def add(x, y):
-        return x + y
+            return x + y
 
+    def subtract(x, y):
+            return x - y
+
+    def square(x):
+            return x * x
+
+    def multiply(x, y):
+            return x * y
+
+    #### Basic input demands for user.
 
     print("Select a function")
     print("1.  - ADD -")
-    print("2.  - FUNCTION -")
+    print("2.  - subtract -".upper())
+    print("3.  - SQUARE -")
+    print("4.  - MULTIPLY -")
 
-    choice = input("Enter a command, (1/2)")
+    #### Input for choosing a mode
 
-    num1 = float(input("Enter a number"))
-    num2 = float(input("Enter another number"))
+    choice = input("Enter a command, (1/2/3)")
+
+    #### This is where the user enters the numbers they are using on the calculator
+
+    num1 = float(input("Enter a number: "))
+    num2 = float(input("Enter another number:  \n\n(if anothr number is not required then just enter any number it will be ignored by the code)"))
 
     if choice == '1':
-        print(num1, "+", num2, "=", add(num1, num2))
+             print(num1, "+", num2, "=", add(num1, num2))
 
+    elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
 
+    elif choice == '3':
+            print(num1, "^2", "=", square(num1))
 
+    elif choice == '4':
+            print(num1, "*", num2, "=", multiply(num1, num2))
 
+restart = input("Do you wish to restart? [y/n]")
+if restart == "y":
+     main()
 
-
-
-
-
-
-
-
+else:
+    exit()
 
 
 
