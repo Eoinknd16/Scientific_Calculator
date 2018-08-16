5dc77abd9216fcf6649254c41a70365314f2e797
Eoinknd16 <42419154+Eoinknd16@users.noreply.github.com>
2018-08-16 18:55:09 +0100

    Update ScientificCalculator.py

--- ScientificCalculator.py
+++ ScientificCalculator.py
@@ -17,6 +17,9 @@ def main():
     def multiply(x, y):
             return x * y
 
+    def exp(x,y):
+            return x ** y
+
     #### Basic input demands for user.
 
     print("Select a function")
@@ -24,27 +27,36 @@ def main():
     print("2.  - subtract -".upper())
     print("3.  - SQUARE -")
     print("4.  - MULTIPLY -")
+    print("5.  - EXPONENTIATE")
 
     #### Input for choosing a mode
 
-    choice = input("Enter a command, (1/2/3)")
+    choice = input("Enter a command, (1/2/3/4/5)")
 
     #### This is where the user enters the numbers they are using on the calculator
 
-    num1 = float(input("Enter a number: "))
-    num2 = float(input("Enter another number:  \n\n(if anothr number is not required then just enter any number it will be ignored by the code)"))
+   # num1 = float(input("Enter a number: "))
+   # num2 = float(input("Enter another number:  \n\n(if anothr number is not required then just enter any number it will be ignored by the code)"))
 
     if choice == '1':
-             print(num1, "+", num2, "=", add(num1, num2))
+        num1 = float(input("Enter a number: "))
+        num2 = float(input("Enter another number: "))
+        print(num1, "+", num2, "=", add(num1, num2))
 
     elif choice == '2':
-            print(num1, "-", num2, "=", subtract(num1, num2))
+        print(num1, "-", num2, "=", subtract(num1, num2))
 
     elif choice == '3':
-            print(num1, "^2", "=", square(num1))
+        num1 = float(input("Enter a number: "))
+        print(num1, "^2", "=", square(num1))
 
     elif choice == '4':
-            print(num1, "*", num2, "=", multiply(num1, num2))
+        num1 = float(input("Enter a number: "))
+        print(num1, "*", num2, "=", multiply(num1, num2))
+
+    elif choice == '5':
+        num1 = float(input("Enter a number: "))
+        print(num1, "**", num2, "=", exp(num1,num2))
 
 restart = input("Do you wish to restart? [y/n]")
 if restart == "y":
